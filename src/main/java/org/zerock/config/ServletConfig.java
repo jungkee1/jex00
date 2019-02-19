e5ecfb075cc9a5f2ff5b45acfe10a0860494a542
coding in pain <45281008+jungkee1@users.noreply.github.com>
2019-02-19 18:24:23 +0900

    Initial commit

---
+++ src/main/java/org/zerock/config/ServletConfig.java
@@ -0,0 +1,24 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages={"org.zerock.jex00"}) //2¹ø
+public class ServletConfig implements WebMvcConfigurer{
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		registry.viewResolver(bean);
+	}
+
+
+}
---
+++ src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{ 
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"}; // root를 찾아라 HomeController로 가는 것임 즉
+	}
+
+}
